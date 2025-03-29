c6b96879946e5e6ae4c64d8657aa2d397352c6ab
Phạm Việt Hoàng - Hoàng PV1 <hoangpv1@bsscommerce.com>
2025-03-29 10:01:43 +0700

    Seed admin

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $adminRole = Role::firstOrCreate(
+            ['code' => 'admin'],
+            ['name' => 'Admin']
+        );
+
+        User::firstOrCreate(
+            ['username' => 'admin'],
+            [
+                'password'                 => Hash::make('admin'),
+                'name'                     => 'Admin',
+                'phone_number'             => '0989407376',
+                'email'                    => 'admin@gmail.com',
+                'status'                   => 'active',
+                'role_id'                  => $adminRole->id,
+                'avatar_url'               => null,
+                'notification_preferences' => null,
+            ]
+        );
+    }
+}
